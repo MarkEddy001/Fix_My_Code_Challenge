bf95b827210aa9cb2830c2bddcf6d172bd916667
MarkEddy001 <Markeddy254@gmail.com>
2023-12-14 05:36:27 -0800

    issue with your delete_dnodeint_at_index function

--- 0x00-challenge/4-delete_dnodeint/delete_dnodeint_at_index.c
+++ 0x00-challenge/4-delete_dnodeint/delete_dnodeint_at_index.c
@@ -11,43 +11,44 @@
  */
 int delete_dnodeint_at_index(dlistint_t **head, unsigned int index)
 {
-	dlistint_t *saved_head;
-	dlistint_t *tmp;
-	unsigned int p;
+        dlistint_t *saved_head;
+        dlistint_t *tmp;
+        unsigned int p;
 
-	if (*head == NULL)
-	{
-		return (-1);
-	}
-	saved_head = *head;
-	p = 0;
-	while (p < index && *head != NULL)
-	{
-		*head = (*head)->next;
-		p++;
-	}
-	if (p != index)
-	{
-		*head = saved_head;
-		return (-1);
-	}
-	if (0 == index)
-	{
-		tmp = (*head)->next;
-		free(*head);
-		*head = tmp;
-		if (tmp != NULL)
-		{
-			tmp->prev = NULL;
-		}
-	}
-	else
-	{
-		(*head)->prev->prev = (*head)->next;
-		free(*head);
-		if ((*head)->next)
-			(*head)->next->prev = (*head)->prev;
-		*head = saved_head;
-	}
-	return (1);
+        if (*head == NULL)
+        {
+                return (-1);
+        }
+        saved_head = *head;
+        p = 0;
+        while (p < index && *head != NULL)
+        {
+                *head = (*head)->next;
+                p++;
+        }
+        if (p != index || *head == NULL)
+        {
+                *head = saved_head;
+                return (-1);
+        }
+        if (0 == index)
+        {
+                tmp = (*head)->next;
+                free(*head);
+                *head = tmp;
+                if (tmp != NULL)
+                {
+                        tmp->prev = NULL;
+                }
+        }
+        else
+        {
+                tmp = *head;
+                tmp->prev->next = tmp->next;
+                if (tmp->next)
+                        tmp->next->prev = tmp->prev;
+                free(tmp);
+                *head = saved_head;
+        }
+        return (1);
 }
