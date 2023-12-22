2c56a9ef8e3f7f4b1ff1ae17274dc80a7b6cd302
MarkEddy001 <Markeddy254@gmail.com>
2023-12-21 22:32:11 -0800

    Updated

--- 0x00-challenge/4-delete_dnodeint/delete_dnodeint_at_index.c
+++ 0x00-challenge/4-delete_dnodeint/delete_dnodeint_at_index.c
@@ -11,53 +11,43 @@
  */
 int delete_dnodeint_at_index(dlistint_t **head, unsigned int index)
 {
-    dlistint_t *saved_head, *tmp;
-    unsigned int p;
+	dlistint_t *saved_head;
+	dlistint_t *tmp;
+	unsigned int p;
 
-    if (*head == NULL)
-    {
-        return (-1);
-    }
-    saved_head = *head;
-    p = 0;
-
-    while (p < index && *head != NULL)
-    {
-        *head = (*head)->next;
-        p++;
-    }
-
-    if (p != index)
-    {
-        *head = saved_head;
-        return (-1);
-    }
-
-    if (index == 0)
-    {
-        tmp = (*head)->next;
-
-        if (tmp != NULL)
-        {
-            tmp->prev = NULL;
-        }
-
-        free(*head);
-        *head = tmp;
-    }
-    else
-    {
-        tmp = *head;
-        tmp->prev->next = tmp->next;
-
-        if (tmp->next != NULL)
-        {
-            tmp->next->prev = tmp->prev;
-        }
-
-        free(tmp);
-        *head = saved_head;
-    }
-
-    return (1);
+	if (*head == NULL)
+	{
+		return (-1);
+	}
+	saved_head = *head;
+	p = 0;
+	while (p < index && *head != NULL)
+	{
+		*head = (*head)->next;
+		p++;
+	}
+	if (p != index)
+	{
+		*head = saved_head;
+		return (-1);
+	}
+	if (0 == index)
+	{
+		tmp = (*head)->next;
+		free(*head);
+		*head = tmp;
+		if (tmp != NULL)
+		{
+			tmp->prev = NULL;
+		}
+	}
+	else
+	{
+		(*head)->prev->next = (*head)->next;
+		free(*head);
+		if ((*head)->next)
+			(*head)->next->prev = (*head)->prev;
+		*head = saved_head;
+	}
+	return (1);
 }
