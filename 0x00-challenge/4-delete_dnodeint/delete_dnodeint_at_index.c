39893cb7a4977df0c8cd8eb933eecd0ccc509555
MarkEddy001 <Markeddy254@gmail.com>
2023-12-21 22:22:38 -0800

    Update

--- 0x00-challenge/4-delete_dnodeint/delete_dnodeint_at_index.c
+++ 0x00-challenge/4-delete_dnodeint/delete_dnodeint_at_index.c
@@ -11,44 +11,52 @@
  */
 int delete_dnodeint_at_index(dlistint_t **head, unsigned int index)
 {
-        dlistint_t *saved_head;
-        dlistint_t *tmp;
-        unsigned int p;
+    dlistint_t *saved_head, *tmp;
+    unsigned int p;
 
-        if (*head == NULL)
+    if (*head == NULL)
+    {
+        return (-1);
+    }
+
+    saved_head = *head;
+    p = 0;
+
+    while (p < index && *head != NULL)
+    {
+        *head = (*head)->next;
+        p++;
+    }
+
+    if (p != index || *head == NULL)
+    {
+        *head = saved_head;
+        return (-1);
+    }
+
+    if (index == 0)
+    {
+        tmp = (*head)->next;
+        free(*head);
+        *head = tmp;
+        if (tmp != NULL)
         {
-                return (-1);
+            tmp->prev = NULL;
         }
-        saved_head = *head;
-        p = 0;
-        while (p < index && *head != NULL)
+    }
+    else
+    {
+        tmp = *head;
+        tmp->prev->next = tmp->next;
+
+        if (tmp->next != NULL)
         {
-                *head = (*head)->next;
-                p++;
+            tmp->next->prev = tmp->prev;
         }
-        if (p != index || *head == NULL)
-        {
-                *head = saved_head;
-                return (-1);
-        }
-        if (0 == index)
-        {
-                tmp = (*head)->next;
-                free(*head);
-                *head = tmp;
-                if (tmp != NULL)
-                {
-                        tmp->prev = NULL;
-                }
-        }
-        else
-        {
-                tmp = *head;
-                tmp->prev->next = tmp->next;
-                if (tmp->next)
-                        tmp->next->prev = tmp->prev;
-                free(tmp);
-                *head = saved_head;
-        }
-        return (1);
+
+        free(tmp);
+        *head = saved_head;
+    }
+
+    return (1);
 }
