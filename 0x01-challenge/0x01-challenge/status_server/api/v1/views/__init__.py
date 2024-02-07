31909de70e2d8cda24a1372e8a91596bb2e2b1a6
MarkEddy001 <markeddy254@gmail.com>
2024-02-07 02:31:16 -0800

    Updated an error

--- 0x01-challenge/0x01-challenge/status_server/api/v1/views/__init__.py
+++ 0x01-challenge/0x01-challenge/status_server/api/v1/views/__init__.py
@@ -2,9 +2,6 @@
 """ Views module
 """
 from flask import Blueprint
-
-# to fix the code, i changed url_prefix from /api/v1 to / because 
-# this indicates the root of the routes
-app_views = Blueprint("app_views", __name__, url_prefix="/")
+app_views = Blueprint("app_views", __name__, url_prefix="/api/v1")
 
 from api.v1.views.index import *
