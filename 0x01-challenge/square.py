46341c194f7df299542769c5df334de846c6cf3c
MarkEddy001 <markeddy254@gmail.com>
2024-02-07 03:18:59 -0800

    fixed Naming Convention, area calculation and perimeter calculation

--- 0x01-challenge/square.py
+++ 0x01-challenge/square.py
@@ -3,50 +3,48 @@
 class Square:
     """
     A class representing a square shape.
-    
     Attributes:
-    - side: The side of the square.
+    - width: The width of the square.
+    - height: The height of the square.
     """
 
-    def __init__(self, side=0):
+    def __init__(self, width=0, height=0):
         """
-        Initializes a square with the given side.
-        
+        Initializes a square with the given width and height.
         Args:
-        - side: The side of the square.
+        - width: The width of the square.
+        - height: The height of the square.
         """
-        self.side = side
+        self.width = width
+        self.height = height
 
     def area_of_my_square(self):
         """
         Calculates the area of the square.
-        
         Returns:
         The area of the square.
         """
-        return self.side * self.side
+        return self.width * self.height
 
     def perimeter_of_my_square(self):
         """
         Calculates the perimeter of the square.
-        
         Returns:
         The perimeter of the square.
         """
-        return self.side * 4
+        return (self.width * 2) + (self.height * 2)
 
     def __str__(self):
         """
         Returns a string representation of the square.
-        
         Returns:
-        A string representation of the square in the format 'Side: side'.
+        A string representation of the square in the format 'width/height'.
         """
-        return "Side: {}".format(self.side)
+        return "{}/{}".format(self.width, self.height)
+
 
 if __name__ == "__main__":
-    s = Square(side=12)
+    s = Square(width=12, height=9)
     print(s)
     print(s.area_of_my_square())
     print(s.perimeter_of_my_square())
-
