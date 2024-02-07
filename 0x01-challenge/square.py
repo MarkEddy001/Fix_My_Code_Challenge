b41888741694abd6bf3706298c863cac81248113
MarkEddy001 <markeddy254@gmail.com>
2024-02-07 02:55:00 -0800

    fixed Naming Convention, area calculation and perimeter calculation

--- 0x01-challenge/square.py
+++ 0x01-challenge/square.py
@@ -1,28 +1,26 @@
 #!/usr/bin/python3
 
-class square():
+class Square():
     
-    width = 0
-    height = 0
+    side = 0
 
-    
     def __init__(self, *args, **kwargs):
         for key, value in kwargs.items():
             setattr(self, key, value)
 
     def area_of_my_square(self):
         """ Area of the square """
-        return self.width * self.width
+        return self.side * self.side
 
-    def PermiterOfMySquare(self):
-        return (self.width * 2) + (self.height * 2)
+    def perimeter_of_my_square(self):
+        return self.side * 4
 
     def __str__(self):
-        return "{}/{}".format(self.width, self.height)
+        return "Side: {}".format(self.side)
 
 if __name__ == "__main__":
 
-    s = square(width=12, height=9)
+    s = Square(side=12)
     print(s)
     print(s.area_of_my_square())
-    print(s.PermiterOfMySquare())
+    print(s.perimeter_of_my_square())
